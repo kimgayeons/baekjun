b84c2f815d357929009ccfc63215701a4eec110e
kimgayeon <4402aaa@gmail.com>
2025-08-27 11:20:49 +0900

    step5 문자열 문제들

---
+++ src/step5/No2743.java
@@ -0,0 +1,11 @@
+package step5;
+
+import java.util.Scanner;
+
+public class No2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        System.out.println(x.length());
+    }
+}
---
+++ src/step5/No27866.java
@@ -0,0 +1,12 @@
+package step5;
+
+import java.util.Scanner;
+
+public class No27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        int num = sc.nextInt();
+        System.out.println(x.charAt(num-1));
+    }
+}
